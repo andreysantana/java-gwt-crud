f9ada0f406bf108c0fbe85a1a8d7da11167b38c1
Andrey de Santana Silva <andreysantana@outlook.com>
2021-04-30 09:27:08 -0300

    Entidade Pessoa

---
+++ src/main/java/com/santana/gwtblue/entidades/Pessoa.java
@@ -0,0 +1,81 @@
+package com.santana.gwtblue.entidades;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+	private int idade;
+	private char sexo = 'M';
+	private String endereco;
+	
+	public Pessoa() {
+		
+	}
+
+	public Pessoa(String nome, int idade, char sexo, String endereco) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.sexo = sexo;
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/com/santana/gwtblue/recursos/PessoaResource.java
@@ -0,0 +1,19 @@
+package com.santana.gwtblue.recursos;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.santana.gwtblue.entidades.Pessoa;
+
+@RestController
+@RequestMapping(value = "/pessoas")
+public class PessoaResource {
+	
+	@GetMapping
+	public ResponseEntity<Pessoa> buscarTodos() {
+		Pessoa p = new Pessoa("Andrey de Santana", 32, 'M', "Rua Bogotá");
+		return ResponseEntity.ok().body(p);
+	}
+}
