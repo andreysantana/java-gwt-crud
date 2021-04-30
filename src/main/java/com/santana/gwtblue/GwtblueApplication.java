f84e0ec908851fa08c9d347b3b40828474e72cc9
Andrey de Santana Silva <andreysantana@outlook.com>
2021-04-30 09:09:59 -0300

    Init project

---
+++ src/main/java/com/santana/gwtblue/GwtblueApplication.java
@@ -0,0 +1,13 @@
+package com.santana.gwtblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwtblueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwtblueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santana/gwtblue/GwtblueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santana.gwtblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwtblueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
